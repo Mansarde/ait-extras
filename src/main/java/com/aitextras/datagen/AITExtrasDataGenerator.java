3bff76610dcb56673a703978e0ab496a0b7a74f5
Addi3 <addiec787@gmail.com>
2025-02-10 15:36:20 +0000

    feat: pink,red,yellow FOB sonics

--- src/main/java/com/aitextras/datagen/AITExtrasDataGenerator.java
+++ src/main/java/com/aitextras/datagen/AITExtrasDataGenerator.java
@@ -44,57 +44,59 @@ public class AITExtrasDataGenerator implements DataGeneratorEntrypoint {
 
     private void genEnglish(FabricDataGenerator.Pack pack) {
         pack.addProvider((((output, registriesFuture) -> {
-            SakitusLanguageProvider provider = new SakitusLanguageProvider(output, LanguageType.EN_US);
+                    SakitusLanguageProvider provider = new SakitusLanguageProvider(output, LanguageType.EN_US);
 
-            //desktops
-            provider.addTranslation("desktop.ait-extras.axos","Axos");
-            provider.addTranslation("desktop.ait-extras.cherryblossom","CherryBlossom");
-            provider.addTranslation("desktop.ait-extras.definitive","Definitive");
-            provider.addTranslation("desktop.ait-extras.exile","Exile");
-            provider.addTranslation("desktop.ait-extras.industrial","Industrial");
-            provider.addTranslation("desktop.ait-extras.tron","Tron");
-            provider.addTranslation("desktop.ait-extras.war_games","War Games");
-            provider.addTranslation("desktop.ait-extras.academic","Academic");
-            provider.addTranslation("desktop.ait-extras.modern_victorian","Modern Victorian");
-            provider.addTranslation("desktop.ait-extras.rebel","Rebel");
-            provider.addTranslation("desktop.ait-extras.undercroft","Undercroft");
-            provider.addTranslation("desktop.ait-extras.tardim","Tardim");
-            provider.addTranslation("desktop.ait-extras.type_85","Type 85");
-            provider.addTranslation("desktop.ait-extras.type_89","Type 89");
+                    //desktops
+                    provider.addTranslation("desktop.ait-extras.axos", "Axos");
+                    provider.addTranslation("desktop.ait-extras.cherryblossom", "CherryBlossom");
+                    provider.addTranslation("desktop.ait-extras.definitive", "Definitive");
+                    provider.addTranslation("desktop.ait-extras.exile", "Exile");
+                    provider.addTranslation("desktop.ait-extras.industrial", "Industrial");
+                    provider.addTranslation("desktop.ait-extras.tron", "Tron");
+                    provider.addTranslation("desktop.ait-extras.war_games", "War Games");
+                    provider.addTranslation("desktop.ait-extras.academic", "Academic");
+                    provider.addTranslation("desktop.ait-extras.modern_victorian", "Modern Victorian");
+                    provider.addTranslation("desktop.ait-extras.rebel", "Rebel");
+                    provider.addTranslation("desktop.ait-extras.undercroft", "Undercroft");
+                    provider.addTranslation("desktop.ait-extras.tardim", "Tardim");
+                    provider.addTranslation("desktop.ait-extras.type_85", "Type 85");
+                    provider.addTranslation("desktop.ait-extras.type_89", "Type 89");
 
-            //exteriors
+                    //exteriors
 
-                //bookshelf
-            provider.addTranslation("exterior.ait-extras.bookshelf_birch","Birch");
-            provider.addTranslation("exterior.ait-extras.bookshelf_cherry","Cherry");
-            provider.addTranslation("exterior.ait-extras.bookshelf_dark_oak","Dark Oak");
-            provider.addTranslation("exterior.ait-extras.bookshelf_jungle","Jungle");
-            provider.addTranslation("exterior.ait-extras.bookshelf_mangrove","Mangrove");
-            provider.addTranslation("exterior.ait-extras.bookshelf_oak","Oak");
-            provider.addTranslation("exterior.ait-extras.bookshelf_spruce","Spruce");
+                        //bookshelf
+                    provider.addTranslation("exterior.ait-extras.bookshelf_birch", "Birch");
+                    provider.addTranslation("exterior.ait-extras.bookshelf_cherry", "Cherry");
+                    provider.addTranslation("exterior.ait-extras.bookshelf_dark_oak", "Dark Oak");
+                    provider.addTranslation("exterior.ait-extras.bookshelf_jungle", "Jungle");
+                    provider.addTranslation("exterior.ait-extras.bookshelf_mangrove", "Mangrove");
+                    provider.addTranslation("exterior.ait-extras.bookshelf_oak", "Oak");
+                    provider.addTranslation("exterior.ait-extras.bookshelf_spruce", "Spruce");
 
-                //modern box
-            provider.addTranslation("exterior.ait-extras.aperture_science","Aperture");
-            provider.addTranslation("exterior.ait-extras.black_mesa","Mesa");
-            provider.addTranslation("exterior.ait-extras.copper","Copper");
-            provider.addTranslation("exterior.ait-extras.copper_toyota","Copper Toyota");
-            provider.addTranslation("exterior.ait-extras.copper_toyota_alternative","Copper Toyota (alt)");
-            provider.addTranslation("exterior.ait-extras.coral_alternative","Coral (alt)");
-            provider.addTranslation("exterior.ait-extras.tokamak_eotd","Tokamak (EOTD)");
-            provider.addTranslation("exterior.ait-extras.toyota_alternative","Toyota (alt)");
-            provider.addTranslation("exterior.ait-extras.toyota_memorial","Memorial");
-            provider.addTranslation("exterior.ait-extras.yard","73 Yards");
+                        //modern box
+                    provider.addTranslation("exterior.ait-extras.aperture_science", "Aperture");
+                    provider.addTranslation("exterior.ait-extras.black_mesa", "Mesa");
+                    provider.addTranslation("exterior.ait-extras.copper", "Copper");
+                    provider.addTranslation("exterior.ait-extras.copper_toyota", "Copper Toyota");
+                    provider.addTranslation("exterior.ait-extras.copper_toyota_alternative", "Copper Toyota (alt)");
+                    provider.addTranslation("exterior.ait-extras.coral_alternative", "Coral (alt)");
+                    provider.addTranslation("exterior.ait-extras.tokamak_eotd", "Tokamak (EOTD)");
+                    provider.addTranslation("exterior.ait-extras.toyota_alternative", "Toyota (alt)");
+                    provider.addTranslation("exterior.ait-extras.toyota_memorial", "Memorial");
+                    provider.addTranslation("exterior.ait-extras.yard", "73 Yards");
 
-                //classic box
-            provider.addTranslation("exterior.ait-extras.tango","Tango");
+                        //classic box
+                    provider.addTranslation("exterior.ait-extras.tango", "Tango");
 
-                //booth
-            provider.addTranslation("exterior.ait-extras.tiny_link","Tiny Link");
+                        //booth
+                    provider.addTranslation("exterior.ait-extras.tiny_link", "Tiny Link");
 
 
-            //sonics
-            provider.addTranslation("sonic.ait-extras.Cane","Cane");
-
+                    //sonics
+                    provider.addTranslation("sonic.ait-extras.cane", "Cane");
+                    provider.addTranslation("sonic.ait-extras.fob_pink", "FOB (Pink)");
+                    provider.addTranslation("sonic.ait-extras.fob_red", "FOB (Red)");
+                    provider.addTranslation("sonic.ait-extras.fob_yellow", "FOB (Yellow)");
 
 
             //provider.translateBlocks(AITExtrasBlocks.class);
