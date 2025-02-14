9ea06242f67bacc26e0bbec45d0a622dcd930c9a
Addi3 <addiec787@gmail.com>
2025-02-14 16:51:00 +0000

    fix: ecto exterior texture / model issues

--- src/main/java/com/aitextras/client/models/doors/EctoDoorModel.java
+++ src/main/java/com/aitextras/client/models/doors/EctoDoorModel.java
@@ -54,12 +54,12 @@ public class EctoDoorModel extends DoorModel {
     public void renderWithAnimations(AbstractLinkableBlockEntity doorEntity, ModelPart root, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float pAlpha) {
         if (!AITMod.CONFIG.CLIENT.ANIMATE_DOORS) {
             DoorHandler door = doorEntity.tardis().get().door();
-            this.TARDIS.getChild("Doors").getChild("left_door").yaw = !door.isLeftOpen() && !door.isOpen() ? 0.0F : 5.0F;
-            this.TARDIS.getChild("Doors").getChild("right_door").yaw = !door.isRightOpen() && !door.areBothOpen() ? 0.0F : -5.0F;
+            this.TARDIS.getChild("Doors").getChild("left_door").yaw = !door.isLeftOpen() && !door.isOpen() ? 0.0F : -5.0F;
+            this.TARDIS.getChild("Doors").getChild("right_door").yaw = !door.isRightOpen() && !door.areBothOpen() ? 0.0F : 5.0F;
         } else {
             float maxRot = 90.0F;
-            this.TARDIS.getChild("Doors").getChild("left_door").yaw = (float)-Math.toRadians((double)(maxRot * doorEntity.tardis().get().door().getLeftRot()));
-            this.TARDIS.getChild("Doors").getChild("right_door").yaw = (float)(Math.toRadians((double)(maxRot * doorEntity.tardis().get().door().getRightRot())));
+            this.TARDIS.getChild("Doors").getChild("left_door").yaw = (float)(Math.toRadians((double)(maxRot * doorEntity.tardis().get().door().getLeftRot())));
+            this.TARDIS.getChild("Doors").getChild("right_door").yaw = (float)-Math.toRadians((double)(maxRot * doorEntity.tardis().get().door().getRightRot()));
         }
 
         matrices.push();
--- src/main/java/com/aitextras/client/models/exteriors/EctoExteriorModel.java
+++ src/main/java/com/aitextras/client/models/exteriors/EctoExteriorModel.java
@@ -42,32 +42,32 @@ public class EctoExteriorModel extends ExteriorModel {
 		ModelPartData cube_r3 = Posts.addChild("cube_r3", ModelPartBuilder.create().uv(218, 41).cuboid(-18.0F, -66.0F, -18.0F, 4.0F, 62.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, -1.5708F, 0.0F));
 
 		ModelPartData cube_r4 = Posts.addChild("cube_r4", ModelPartBuilder.create().uv(0, 423).mirrored().cuboid(-9.5F, -29.0F, 2.0F, 15.0F, 52.0F, 0.0F, new Dilation(0.0F)).mirrored(false)
-		.uv(12, 473).mirrored().cuboid(-9.5F, -21.0F, 2.0F, 15.0F, 32.0F, 0.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(25.5F, -35.0F, 23.0F, 0.0F, -0.7854F, 0.0F));
+				.uv(12, 473).mirrored().cuboid(-9.5F, -21.0F, 2.0F, 15.0F, 32.0F, 0.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(25.5F, -35.0F, 23.0F, 0.0F, -0.7854F, 0.0F));
 
 		ModelPartData cube_r5 = Posts.addChild("cube_r5", ModelPartBuilder.create().uv(12, 473).cuboid(-5.5F, -21.0F, -2.0F, 15.0F, 32.0F, 0.0F, new Dilation(0.0F))
-		.uv(0, 423).cuboid(-5.5F, -29.0F, -2.0F, 15.0F, 52.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-25.5F, -35.0F, -23.0F, 0.0F, -0.7854F, 0.0F));
+				.uv(0, 423).cuboid(-5.5F, -29.0F, -2.0F, 15.0F, 52.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-25.5F, -35.0F, -23.0F, 0.0F, -0.7854F, 0.0F));
 
 		ModelPartData Doors = TARDIS.addChild("Doors", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
 		ModelPartData right_door = Doors.addChild("right_door", ModelPartBuilder.create().uv(181, 177).cuboid(0.5F, -29.5F, -0.5F, 13.0F, 55.0F, 1.0F, new Dilation(0.0F))
-		.uv(0, 198).cuboid(0.5F, -29.5F, -1.0F, 14.0F, 55.0F, 0.0F, new Dilation(0.0F))
-		.uv(0, 0).cuboid(11.5F, -10.5F, -1.5F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(-13.5F, -29.5F, -15.5F));
+				.uv(0, 198).cuboid(0.5F, -29.5F, -1.3F, 14.0F, 55.0F, 0.0F, new Dilation(0.0F))
+				.uv(0, 0).cuboid(11.5F, -10.5F, -1.5F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(-13.5F, -29.5F, -15.5F));
 
 		ModelPartData left_door = Doors.addChild("left_door", ModelPartBuilder.create().uv(189, 41).cuboid(-13.5F, -29.5F, -0.5F, 13.0F, 55.0F, 1.0F, new Dilation(0.0F))
-		.uv(0, 0).cuboid(-12.5F, -10.5F, -1.5F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(13.5F, -29.5F, -15.5F, 0.0F, -0.0436F, 0.0F));
+				.uv(0, 0).cuboid(-12.5F, -10.5F, -1.5F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(13.5F, -29.5F, -15.5F, 0.0F, -0.0436F, 0.0F));
 
 		ModelPartData Walls = TARDIS.addChild("Walls", ModelPartBuilder.create().uv(60, 430).mirrored().cuboid(-21.0F, -43.0F, 13.6F, 6.0F, 31.0F, 6.0F, new Dilation(0.0F)).mirrored(false)
-		.uv(129, 15).cuboid(-16.0F, -60.0F, -14.0F, 1.0F, 56.0F, 28.0F, new Dilation(0.0F))
-		.uv(59, 142).cuboid(-16.5F, -60.0F, -14.0F, 0.0F, 56.0F, 28.0F, new Dilation(0.0F))
-		.uv(63, 227).cuboid(-14.0F, -60.0F, -16.0F, 1.0F, 56.0F, 1.0F, new Dilation(0.0F))
-		.uv(116, 170).cuboid(13.0F, -60.0F, -16.0F, 1.0F, 56.0F, 1.0F, new Dilation(0.0F))
-		.uv(115, 0).cuboid(-13.0F, -60.0F, -16.0F, 26.0F, 1.0F, 1.0F, new Dilation(0.0F))
-		.uv(59, 113).cuboid(13.0F, -60.0F, -16.5F, 1.0F, 56.0F, 0.0F, new Dilation(0.0F))
-		.uv(115, 3).cuboid(-13.0F, -60.0F, -16.5F, 26.0F, 1.0F, 0.0F, new Dilation(0.0F))
-		.uv(62, 113).cuboid(-14.0F, -60.0F, -16.5F, 1.0F, 56.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+				.uv(129, 15).cuboid(-16.0F, -60.0F, -14.0F, 1.0F, 56.0F, 28.0F, new Dilation(0.0F))
+				.uv(59, 142).cuboid(-16.5F, -60.0F, -14.0F, 0.0F, 56.0F, 28.0F, new Dilation(0.0F))
+				.uv(63, 227).cuboid(-14.0F, -60.0F, -16.0F, 1.0F, 56.0F, 1.0F, new Dilation(0.0F))
+				.uv(116, 170).cuboid(13.0F, -60.0F, -16.0F, 1.0F, 56.0F, 1.0F, new Dilation(0.0F))
+				.uv(115, 0).cuboid(-13.0F, -60.0F, -16.0F, 26.0F, 1.0F, 1.0F, new Dilation(0.0F))
+				.uv(59, 113).cuboid(13.0F, -60.0F, -16.5F, 1.0F, 56.0F, 0.0F, new Dilation(0.0F))
+				.uv(115, 3).cuboid(-13.0F, -60.0F, -16.5F, 26.0F, 1.0F, 0.0F, new Dilation(0.0F))
+				.uv(62, 113).cuboid(-14.0F, -60.0F, -16.5F, 1.0F, 56.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
 		ModelPartData Wall_r1 = Walls.addChild("Wall_r1", ModelPartBuilder.create().uv(160, 72).cuboid(-16.5F, -60.0F, -14.0F, 0.0F, 56.0F, 28.0F, new Dilation(0.0F))
-		.uv(93, 85).cuboid(-16.0F, -60.0F, -14.0F, 1.0F, 56.0F, 28.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+				.uv(93, 85).cuboid(-16.0F, -60.0F, -14.0F, 1.0F, 56.0F, 28.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
 		ModelPartData Wall_r2 = Walls.addChild("Wall_r2", ModelPartBuilder.create().uv(0, 293).cuboid(0.0F, -9.0F, -5.0F, 0.0F, 12.0F, 10.0F, new Dilation(0.0F)), ModelTransform.of(-5.0F, -52.1F, 18.65F, 1.5708F, 1.309F, 1.5708F));
 
@@ -80,7 +80,7 @@ public class EctoExteriorModel extends ExteriorModel {
 		ModelPartData Wall_r6 = Walls.addChild("Wall_r6", ModelPartBuilder.create().uv(0, 293).cuboid(0.0F, -14.0F, -3.0F, 0.0F, 36.0F, 10.0F, new Dilation(0.0F)), ModelTransform.of(-7.0F, -37.0F, 19.25F, 0.0F, 1.5708F, 0.0F));
 
 		ModelPartData Wall_r7 = Walls.addChild("Wall_r7", ModelPartBuilder.create().uv(124, 142).cuboid(-16.75F, -60.0F, -14.0F, 0.0F, 56.0F, 28.0F, new Dilation(0.0F))
-		.uv(0, 113).cuboid(-16.0F, -60.0F, -14.0F, 1.0F, 56.0F, 28.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 1.5708F, 0.0F));
+				.uv(0, 113).cuboid(-16.0F, -60.0F, -14.0F, 1.0F, 56.0F, 28.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 1.5708F, 0.0F));
 
 		ModelPartData PCB = TARDIS.addChild("PCB", ModelPartBuilder.create().uv(181, 167).cuboid(-17.0F, -64.0F, -19.0F, 34.0F, 5.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -1.0F, 0.0F));
 
@@ -91,37 +91,52 @@ public class EctoExteriorModel extends ExteriorModel {
 		ModelPartData cube_r8 = PCB.addChild("cube_r8", ModelPartBuilder.create().uv(160, 31).cuboid(-17.0F, -61.0F, -19.0F, 34.0F, 5.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -3.0F, 0.0F, 0.0F, -1.5708F, 0.0F));
 
 		ModelPartData Roof = TARDIS.addChild("Roof", ModelPartBuilder.create().uv(0, 43).cuboid(-16.0F, -68.0F, -16.0F, 32.0F, 4.0F, 32.0F, new Dilation(0.0F))
-		.uv(0, 43).cuboid(-17.0F, -67.5F, -17.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
-		.uv(22, 7).cuboid(-17.0F, -67.5F, 14.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
-		.uv(0, 30).cuboid(14.0F, -67.5F, -17.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
-		.uv(28, 305).cuboid(-14.0F, -71.0F, -19.0F, 28.0F, 5.0F, 4.0F, new Dilation(0.0F))
-		.uv(17, 26).cuboid(14.0F, -67.5F, 14.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
-		.uv(0, 80).cuboid(-15.0F, -70.0F, -15.0F, 30.0F, 2.0F, 30.0F, new Dilation(0.0F))
-		.uv(0, 382).cuboid(-10.0F, -76.0F, -13.0F, 20.0F, 6.0F, 26.0F, new Dilation(0.0F))
-		.uv(45, 317).cuboid(-12.0F, -74.0F, -14.0F, 2.0F, 4.0F, 0.0F, new Dilation(0.0F))
-		.uv(45, 317).mirrored().cuboid(10.0F, -74.0F, -14.0F, 2.0F, 4.0F, 0.0F, new Dilation(0.0F)).mirrored(false)
-		.uv(40, 345).cuboid(10.0F, -74.0F, -13.0F, 4.0F, 4.0F, 26.0F, new Dilation(0.0F))
-		.uv(40, 345).mirrored().cuboid(-14.0F, -74.0F, -13.0F, 4.0F, 4.0F, 26.0F, new Dilation(0.0F)).mirrored(false)
-		.uv(0, 0).cuboid(-3.0F, -78.0F, -3.0F, 6.0F, 3.0F, 6.0F, new Dilation(0.0F))
-		.uv(0, 374).cuboid(-4.0F, -75.0F, -4.0F, 8.0F, 5.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -1.0F, 0.0F));
+				.uv(0, 43).cuboid(-17.0F, -67.5F, -17.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
+				.uv(22, 7).cuboid(-17.0F, -67.5F, 14.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
+				.uv(0, 30).cuboid(14.0F, -67.5F, -17.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
+				.uv(17, 26).cuboid(14.0F, -67.5F, 14.0F, 3.0F, 4.0F, 3.0F, new Dilation(0.05F))
+				.uv(0, 80).cuboid(-15.0F, -70.0F, -15.0F, 30.0F, 2.0F, 30.0F, new Dilation(0.0F))
+				.uv(0, 382).cuboid(-10.0F, -76.0F, -13.0F, 20.0F, 6.0F, 26.0F, new Dilation(0.0F))
+				.uv(45, 317).cuboid(-12.0F, -74.0F, -14.0F, 2.0F, 4.0F, 0.0F, new Dilation(0.0F))
+				.uv(45, 317).mirrored().cuboid(10.0F, -74.0F, -14.0F, 2.0F, 4.0F, 0.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(40, 345).cuboid(10.0F, -74.0F, -13.0F, 4.0F, 4.0F, 26.0F, new Dilation(0.0F))
+				.uv(40, 345).mirrored().cuboid(-14.0F, -74.0F, -13.0F, 4.0F, 4.0F, 26.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(0, 0).cuboid(-3.0F, -77.0F, -3.0F, 6.0F, 2.0F, 6.0F, new Dilation(0.0F))
+				.uv(0, 374).cuboid(-4.0F, -75.0F, -4.0F, 8.0F, 5.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -1.0F, 0.0F));
 
-		ModelPartData cube_r9 = Roof.addChild("cube_r9", ModelPartBuilder.create().uv(0, 18).cuboid(-2.0F, -71.75F, -2.0F, 4.0F, 5.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -11.25F, 0.0F, 0.0F, 0.7854F, 0.0F));
+		ModelPartData cube_r9 = Roof.addChild("cube_r9", ModelPartBuilder.create().uv(0, 18).cuboid(-2.0F, -70.75F, -2.0F, 4.0F, 5.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -11.25F, 0.0F, 0.0F, 0.7854F, 0.0F));
 
-		ModelPartData cube_r10 = Roof.addChild("cube_r10", ModelPartBuilder.create().uv(80, 333).cuboid(-3.0F, -2.0F, -1.0F, 6.0F, 5.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(-9.0F, -79.0F, 2.0F, 0.0F, 1.5708F, 0.0F));
+		ModelPartData cube_r10 = Roof.addChild("cube_r10", ModelPartBuilder.create().uv(80, 333).cuboid(-3.0F, -2.0F, -1.0F, 0.0F, 5.0F, 3.0F, new Dilation(0.0F))
+				.uv(86, 333).cuboid(3.0F, -2.0F, -1.0F, 0.0F, 5.0F, 3.0F, new Dilation(0.0F))
+				.uv(80, 333).cuboid(-3.0F, 3.0F, -1.0F, 6.0F, 0.0F, 3.0F, new Dilation(0.0F))
+				.uv(92, 336).cuboid(-3.0F, -2.0F, 2.0F, 6.0F, 5.0F, 0.0F, new Dilation(0.0F))
+				.uv(80, 333).cuboid(-3.0F, -2.0F, -1.0F, 6.0F, 0.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(-9.0F, -79.0F, 2.0F, 0.0F, 1.5708F, 0.0F));
 
 		ModelPartData cube_r11 = Roof.addChild("cube_r11", ModelPartBuilder.create().uv(64, 333).cuboid(-2.0F, -1.0F, -1.0F, 4.0F, 3.0F, 3.0F, new Dilation(0.0F))
-		.uv(68, 324).cuboid(-1.0F, 2.0F, 0.0F, 2.0F, 7.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-6.0F, -79.0F, 2.0F, 0.0F, 1.5708F, 0.0F));
+				.uv(68, 324).cuboid(-1.0F, 2.0F, 0.0F, 2.0F, 7.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-6.0F, -79.0F, 2.0F, 0.0F, 1.5708F, 0.0F));
 
-		ModelPartData cube_r12 = Roof.addChild("cube_r12", ModelPartBuilder.create().uv(80, 333).mirrored().cuboid(-3.0F, -2.0F, -1.0F, 6.0F, 5.0F, 3.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(9.0F, -79.0F, 2.0F, 0.0F, -1.5708F, 0.0F));
+		ModelPartData cube_r12 = Roof.addChild("cube_r12", ModelPartBuilder.create().uv(80, 333).mirrored().cuboid(-3.0F, -2.0F, -1.0F, 6.0F, 0.0F, 3.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(80, 333).mirrored().cuboid(-3.0F, 3.0F, -1.0F, 6.0F, 0.0F, 3.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(80, 333).mirrored().cuboid(-3.0F, -2.0F, -1.0F, 0.0F, 5.0F, 3.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(86, 333).mirrored().cuboid(3.0F, -2.0F, -1.0F, 0.0F, 5.0F, 3.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(92, 336).mirrored().cuboid(-3.0F, -2.0F, 2.0F, 6.0F, 5.0F, 0.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(9.0F, -79.0F, 2.0F, 0.0F, -1.5708F, 0.0F));
 
 		ModelPartData cube_r13 = Roof.addChild("cube_r13", ModelPartBuilder.create().uv(64, 333).mirrored().cuboid(-2.0F, -1.0F, -1.0F, 4.0F, 3.0F, 3.0F, new Dilation(0.0F)).mirrored(false)
-		.uv(68, 324).mirrored().cuboid(-1.0F, 2.0F, 0.0F, 2.0F, 7.0F, 1.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(6.0F, -79.0F, 2.0F, 0.0F, -1.5708F, 0.0F));
+				.uv(68, 324).mirrored().cuboid(-1.0F, 2.0F, 0.0F, 2.0F, 7.0F, 1.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(6.0F, -79.0F, 2.0F, 0.0F, -1.5708F, 0.0F));
 
 		ModelPartData cube_r14 = Roof.addChild("cube_r14", ModelPartBuilder.create().uv(50, 317).mirrored().cuboid(-2.0F, -2.0F, -0.25F, 4.0F, 4.0F, 2.0F, new Dilation(0.0F)).mirrored(false)
-		.uv(51, 324).mirrored().cuboid(-2.0F, -2.0F, -1.25F, 4.0F, 4.0F, 1.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(11.0F, -75.0F, -14.75F, 0.0F, 0.0F, 0.7854F));
+				.uv(51, 324).mirrored().cuboid(-2.0F, 2.0F, -1.25F, 4.0F, 0.0F, 1.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(51, 324).mirrored().cuboid(-2.0F, -2.0F, -1.25F, 4.0F, 0.0F, 1.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(51, 324).mirrored().cuboid(-2.0F, -2.0F, -1.25F, 0.0F, 4.0F, 1.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(55, 324).mirrored().cuboid(2.0F, -2.0F, -1.25F, 0.0F, 4.0F, 1.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(11.0F, -75.0F, -14.75F, 0.0F, 0.0F, 0.7854F));
 
-		ModelPartData cube_r15 = Roof.addChild("cube_r15", ModelPartBuilder.create().uv(51, 324).cuboid(-2.0F, -2.0F, -1.25F, 4.0F, 4.0F, 1.0F, new Dilation(0.0F))
-		.uv(50, 317).cuboid(-2.0F, -2.0F, -0.25F, 4.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(-11.0F, -75.0F, -14.75F, 0.0F, 0.0F, -0.7854F));
+		ModelPartData cube_r15 = Roof.addChild("cube_r15", ModelPartBuilder.create().uv(51, 324).cuboid(-2.0F, -2.0F, -1.25F, 4.0F, 0.0F, 1.0F, new Dilation(0.0F))
+				.uv(51, 324).cuboid(-2.0F, -2.0F, -1.25F, 0.0F, 4.0F, 1.0F, new Dilation(0.0F))
+				.uv(55, 324).cuboid(2.0F, -2.0F, -1.25F, 0.0F, 4.0F, 1.0F, new Dilation(0.0F))
+				.uv(51, 324).cuboid(-2.0F, 2.0F, -1.25F, 4.0F, 0.0F, 1.0F, new Dilation(0.0F))
+				.uv(50, 317).cuboid(-2.0F, -2.0F, -0.25F, 4.0F, 4.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(-11.0F, -75.0F, -14.75F, 0.0F, 0.0F, -0.7854F));
+
+		ModelPartData cube_r16 = Roof.addChild("cube_r16", ModelPartBuilder.create().uv(28, 305).cuboid(-14.0F, -2.5F, 0.0F, 28.0F, 5.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -68.5F, -17.0F, -0.4363F, 0.0F, 0.0F));
 		return TexturedModelData.of(modelData, 512, 512);
 	}
 	@Override
