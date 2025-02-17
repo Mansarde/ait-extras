2cdaa907c79644c0ab956687830c81e01192332a
Addi3 <addiec787@gmail.com>
2025-02-17 02:22:43 +0000

    fix: postbox exterior pivot point

--- src/main/java/com/aitextras/client/models/exteriors/PostBoxExteriorModel.java
+++ src/main/java/com/aitextras/client/models/exteriors/PostBoxExteriorModel.java
@@ -29,25 +29,25 @@ public class PostBoxExteriorModel extends ExteriorModel {
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData bone = modelPartData.addChild("bone", ModelPartBuilder.create().uv(2, 21).cuboid(-21.0F, -29.0F, 0.0F, 1.0F, 28.0F, 13.0F, new Dilation(0.0F))
-				.uv(1, 21).mirrored().cuboid(-8.0F, -29.0F, 0.0F, 1.0F, 28.0F, 13.0F, new Dilation(0.0F)).mirrored(false)
-				.uv(0, 0).cuboid(-22.0F, -33.0F, -2.0F, 16.0F, 2.0F, 16.0F, new Dilation(0.0F))
-				.uv(72, 1).cuboid(-21.0F, -31.0F, -1.0F, 14.0F, 2.0F, 14.0F, new Dilation(0.0F))
-				.uv(28, 20).cuboid(-21.0F, -0.1F, -1.0F, 14.0F, 0.0F, 14.0F, new Dilation(0.0F))
-				.uv(38, 52).cuboid(-22.0F, -2.0F, -2.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
-				.uv(42, 56).cuboid(-22.0F, -2.0F, -1.0F, 2.0F, 2.0F, 13.0F, new Dilation(0.0F))
-				.uv(38, 52).cuboid(-7.0F, -2.0F, -2.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(14.0F, 24.0F, -6.0F));
+		ModelPartData bone = modelPartData.addChild("bone", ModelPartBuilder.create().uv(2, 21).cuboid(-7.0F, -29.0F, -5.8636F, 1.0F, 28.0F, 13.0F, new Dilation(0.0F))
+				.uv(1, 21).mirrored().cuboid(6.0F, -29.0F, -5.8636F, 1.0F, 28.0F, 13.0F, new Dilation(0.0F)).mirrored(false)
+				.uv(0, 0).cuboid(-8.0F, -33.0F, -7.8636F, 16.0F, 2.0F, 16.0F, new Dilation(0.0F))
+				.uv(72, 1).cuboid(-7.0F, -31.0F, -6.8636F, 14.0F, 2.0F, 14.0F, new Dilation(0.0F))
+				.uv(28, 20).cuboid(-7.0F, -0.1F, -6.8636F, 14.0F, 0.0F, 14.0F, new Dilation(0.0F))
+				.uv(38, 52).cuboid(-8.0F, -2.0F, -7.8636F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
+				.uv(42, 56).cuboid(-8.0F, -2.0F, -6.8636F, 2.0F, 2.0F, 13.0F, new Dilation(0.0F))
+				.uv(38, 52).cuboid(7.0F, -2.0F, -7.8636F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, -0.1364F));
 
-		ModelPartData cube_r1 = bone.addChild("cube_r1", ModelPartBuilder.create().uv(42, 75).cuboid(-1.0F, -1.0F, -6.5F, 2.0F, 2.0F, 13.0F, new Dilation(0.0F)), ModelTransform.of(-7.0F, -1.0F, 5.5F, 0.0F, 3.1416F, 0.0F));
+		ModelPartData cube_r1 = bone.addChild("cube_r1", ModelPartBuilder.create().uv(42, 75).cuboid(-1.0F, -1.0F, -6.5F, 2.0F, 2.0F, 13.0F, new Dilation(0.0F)), ModelTransform.of(7.0F, -1.0F, -0.3636F, 0.0F, 3.1416F, 0.0F));
 
-		ModelPartData cube_r2 = bone.addChild("cube_r2", ModelPartBuilder.create().uv(37, 51).cuboid(-8.0F, -2.0F, -2.0F, 16.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(-14.0F, 0.0F, 12.0F, 0.0F, 3.1416F, 0.0F));
+		ModelPartData cube_r2 = bone.addChild("cube_r2", ModelPartBuilder.create().uv(37, 51).cuboid(-8.0F, -2.0F, -2.0F, 16.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 6.1364F, 0.0F, 3.1416F, 0.0F));
 
-		ModelPartData cube_r3 = bone.addChild("cube_r3", ModelPartBuilder.create().uv(2, 22).mirrored().cuboid(-1.0F, -14.5F, -6.0F, 1.0F, 28.0F, 12.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(-14.0F, -14.5F, 13.0F, 0.0F, -1.5708F, 0.0F));
+		ModelPartData cube_r3 = bone.addChild("cube_r3", ModelPartBuilder.create().uv(2, 22).mirrored().cuboid(-1.0F, -14.5F, -6.0F, 1.0F, 28.0F, 12.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(0.0F, -14.5F, 7.1364F, 0.0F, -1.5708F, 0.0F));
 
 		ModelPartData door = bone.addChild("door", ModelPartBuilder.create().uv(34, 63).cuboid(-13.0F, -10.75F, 0.2F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
 				.uv(34, 63).cuboid(-13.0F, -10.75F, -1.2F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F))
 				.uv(39, 45).cuboid(-13.0F, 6.25F, -2.0F, 14.0F, 2.0F, 2.0F, new Dilation(0.0F))
-				.uv(0, 64).cuboid(-13.0F, -20.75F, -1.0F, 14.0F, 28.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, -8.25F, 0.0F));
+				.uv(0, 64).cuboid(-13.0F, -20.75F, -1.0F, 14.0F, 28.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(6.0F, -8.25F, -5.8636F));
 		return TexturedModelData.of(modelData, 128, 128);
 	}
 
