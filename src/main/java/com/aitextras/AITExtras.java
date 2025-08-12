aa27291746537e9df1f20931bdc5f34166b0b3cb
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-08-12 22:47:34 +0100

    chore: bye bye old addon system

--- src/main/java/com/aitextras/AITExtras.java
+++ src/main/java/com/aitextras/AITExtras.java
@@ -2,24 +2,14 @@ package com.aitextras;
 
 import com.aitextras.core.*;
 import com.aitextras.world.gen.AITExtrasWorldGeneration;
-import dev.amble.ait.core.AITSounds;
-import dev.amble.ait.data.schema.exterior.category.CapsuleCategory;
-import dev.amble.ait.data.schema.exterior.category.PoliceBoxCategory;
-import dev.amble.ait.data.schema.exterior.variant.addon.AddonExterior;
 import dev.amble.lib.container.RegistryContainer;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 
 
 public class AITExtras implements ModInitializer {
 
     public static final String MOD_ID = "ait-extras";
-    public static AddonExterior ECTO;
-    public static AddonExterior POSTBOX;
-    public static AddonExterior VANILLA;
-    public static AddonExterior CLOCK;
-    public static AddonExterior ORIGIN;
 
     public static Identifier id(String path) {
         return new Identifier(MOD_ID, path);
@@ -27,168 +17,11 @@ public class AITExtras implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        registerAddonExteriors();
         RegistryContainer.register(AITExtrasItems.class, MOD_ID);
         RegistryContainer.register(AITExtrasBlocks.class, MOD_ID);
         RegistryContainer.register(AITExtrasBlockEntityTypes.class, MOD_ID);
         RegistryContainer.register(AITExtrasItemGroups.class, MOD_ID);
         AITExtrasSounds.init();
         AITExtrasWorldGeneration.generateAITExtrasWorldGen();
-    }
-
-    private void registerAddonExteriors() {
-        //ECTO
-           ECTO = new AddonExterior(PoliceBoxCategory.REFERENCE, MOD_ID, "ecto").register();
-           ECTO.setDoor(new AddonExterior.Door(
-                           ECTO, true, AITSounds.POLICE_BOX_DOOR_OPEN, AITSounds.POLICE_BOX_DOOR_CLOSE))
-                   .toDoor().register();
-           ECTO.hasPortals();
-           ECTO.setPortalTranslations((pos, b) -> {
-               return switch(b) {
-                   case 0 -> pos.add(0, -0.05, -0.628); // NORTH
-                   case 1, 2, 3 -> pos.add(0.43, -0.05, -0.43); // NORTH EAST p n
-                   case 4 -> pos.add(0.628, -0.05, 0); // EAST
-                   case 5, 6, 7 -> pos.add(0.43, -0.05, 0.43); // SOUTH EAST p p
-                   case 8 -> pos.add(0, -0.05, 0.628); // SOUTH
-                   case 9, 10, 11 -> pos.add(-0.43, -0.05, 0.43); // SOUTH WEST n p
-                   case 12 -> pos.add(-0.628, -0.05, 0); // WEST
-                   case 13, 14, 15 -> pos.add(-0.43, -0.05, -0.43); // NORTH WEST n n
-                   default -> pos;
-               };
-           });
-           ECTO.toDoor().setPortalTranslations((pos, b) -> {
-               return switch(b) {
-                   case DOWN, UP -> pos;
-                   case NORTH -> pos.add(0, 0.05, -0.45);
-                   case SOUTH -> pos.add(0, 0.05, 0.45);
-                   case WEST -> pos.add(-0.45, 0.05, 0);
-                   case EAST -> pos.add(0.45, 0.05, 0);
-               };
-           });
-
-     //POSTBOX
-            POSTBOX = new AddonExterior(new Identifier(MOD_ID, "post_box"), MOD_ID, "post_box").register();
-            POSTBOX.setDoor(new AddonExterior.Door(
-                         POSTBOX, false, net.minecraft.sound.SoundEvents.BLOCK_WOODEN_TRAPDOOR_OPEN,
-                         net.minecraft.sound.SoundEvents.BLOCK_WOODEN_TRAPDOOR_CLOSE))
-                    .toDoor().register();
-             POSTBOX.hasPortals();
-             POSTBOX.setPortalTranslations((pos, b) -> {
-             return switch(b) {
-                 case 0 -> pos.add(0, -0.035, -0.4); // NORTH
-                    case 1, 2, 3 -> pos.add(0.43, -0.035, -0.5); // NORTH EAST p n
-                    case 4 -> pos.add(0.428, -0.035, 0); // EAST
-                    case 5, 6, 7 -> pos.add(0.23, -0.035, 0.5); // SOUTH EAST p p
-                    case 8 -> pos.add(0, -0.035, 0.424); // SOUTH
-                    case 9, 10, 11 -> pos.add(-0.23, -0.035, 0.5); // SOUTH WEST n p
-                    case 12 -> pos.add(-0.428, -0.235, 0); // WEST
-                 case 13, 14, 15 -> pos.add(-0.23, -0.035, -0.5); // NORTH WEST n n
-                 default -> pos;
-             };
-            });
-
-         POSTBOX.toDoor().setPortalTranslations((pos, b) -> {
-                return switch(b) {
-                   case DOWN, UP -> pos;
-                   case NORTH -> pos.add(-0.1, 0.075, -0.38);
-                   case SOUTH -> pos.add(0, 0.075, 0.47);
-                  case WEST -> pos.add(-0.47, 0.075, 0);
-                  case EAST -> pos.add(0.47, 0.075, 0);
-              };
-         });
-
-        //VANILLA
-            VANILLA = new AddonExterior(new Identifier(MOD_ID, "vanilla"), MOD_ID, "vanilla").register();
-            VANILLA.setDoor(new AddonExterior.Door(
-                         VANILLA, false, SoundEvents.BLOCK_IRON_DOOR_OPEN,
-                         SoundEvents.BLOCK_IRON_DOOR_CLOSE))
-                .toDoor().register();
-            VANILLA.hasPortals();
-            VANILLA.setPortalTranslations((pos, b) -> {
-             return switch(b) {
-                    case 0 -> pos.add(0.1, -0.05, -0.42); // NORTH
-                    case 1, 2, 3 -> pos.add(0.43, -0.05, -0.32); // NORTH EAST p n
-                    case 4 -> pos.add(0.428, -0.05, 0); // EAST
-                    case 5, 6, 7 -> pos.add(0.23, -0.05, 0.32); // SOUTH EAST p p
-                    case 8 -> pos.add(0, -0.25, 0.428); // SOUTH
-                   case 9, 10, 11 -> pos.add(-0.23, -0.05, 0.32); // SOUTH WEST n p
-                   case 12 -> pos.add(-0.428, -0.25, 0); // WEST
-                   case 13, 14, 15 -> pos.add(-0.23, -0.05, -0.32); // NORTH WEST n n
-                   default -> pos;
-                };
-         });
-          VANILLA.toDoor().setPortalTranslations((pos, b) -> {
-                return switch(b) {
-                 case DOWN, UP -> pos;
-                    case NORTH -> pos.add(0, 0.075, -0.4);
-                    case SOUTH -> pos.add(0, 0.075, 0.4);
-                    case WEST -> pos.add(-0.47, 0.075, 0);
-                    case EAST -> pos.add(0.47, 0.075, 0);
-                };
-         });
-
-
-
-        //CLOCK
-        CLOCK = new AddonExterior(new Identifier(MOD_ID, "clock"), MOD_ID, "clock").register();
-        CLOCK.setDoor(new AddonExterior.Door(
-                        CLOCK, false, SoundEvents.BLOCK_WOODEN_TRAPDOOR_OPEN,
-                        SoundEvents.BLOCK_WOODEN_TRAPDOOR_CLOSE))
-                .toDoor().register();
-        CLOCK.hasPortals();
-        CLOCK.setPortalTranslations((pos, b) -> {
-            return switch(b) {
-                case 0 -> pos.add(0, -0.35, -0.37); // NORTH
-                case 1, 2, 3 -> pos.add(0, -0.35, -0.37); // NORTH EAST p n
-                case 4 -> pos.add(0, -0.35, 0); // EAST
-                case 5, 6, 7 -> pos.add(0, -0.35, 0.37); // SOUTH EAST p p
-                case 8 -> pos.add(0, -0.35, 0.37); // SOUTH
-                case 9, 10, 11 -> pos.add(-0, -0.35, 0.37); // SOUTH WEST n p
-                case 12 -> pos.add(-0, -0.35, 0); // WEST
-                case 13, 14, 15 -> pos.add(-0, -0.35, -0.37); // NORTH WEST n n
-                default -> pos;
-            };
-        });
-        CLOCK.toDoor().setPortalTranslations((pos, b) -> {
-            return switch(b) {
-                case DOWN, UP -> pos;
-                case NORTH -> pos.add(0, -0.2, -0.4);
-                case SOUTH -> pos.add(0, -0.2, 0.4);
-                case WEST -> pos.add(-0, -0.2, 0);
-                case EAST -> pos.add(0, -0.2, 0);
-            };
-        });
-
-
-
-        //ORIGIN
-        ORIGIN = new AddonExterior(new Identifier(MOD_ID, "origin"), MOD_ID, "origin").register();
-        ORIGIN.setDoor(new AddonExterior.Door(
-                        ORIGIN, false, SoundEvents.BLOCK_IRON_DOOR_OPEN,
-                        SoundEvents.BLOCK_IRON_DOOR_CLOSE))
-                .toDoor().register();
-        ORIGIN.hasPortals();
-        ORIGIN.setPortalTranslations((pos, b) -> {
-            return switch(b) {
-                case 0 -> pos.add(0, -0.04, -0.37); // NORTH
-                case 1, 2, 3 -> pos.add(0, -0.04, -0.37); // NORTH EAST p n
-                case 4 -> pos.add(0, -0.04, 0); // EAST
-                case 5, 6, 7 -> pos.add(0, -0.04, 0.37); // SOUTH EAST p p
-                case 8 -> pos.add(0, -0.04, 0.37); // SOUTH
-                case 9, 10, 11 -> pos.add(-0, -0.04, 0.37); // SOUTH WEST n p
-                case 12 -> pos.add(-0, -0.04, 0); // WEST
-                case 13, 14, 15 -> pos.add(-0, -0.04, -0.37); // NORTH WEST n n
-                default -> pos;
-            };
-        });
-        ORIGIN.toDoor().setPortalTranslations((pos, b) -> {
-            return switch(b) {
-                case DOWN, UP -> pos;
-                case NORTH -> pos.add(0.1, 0.05, -0.3);
-                case SOUTH -> pos.add(0.1, 0.05, 0.3);
-                case WEST -> pos.add(-0, 0.05, 0);
-                case EAST -> pos.add(0, 0.05, 0);
-            };
-        });
     }
 }
--- src/main/java/com/aitextras/client/AITExtrasClient.java
+++ src/main/java/com/aitextras/client/AITExtrasClient.java
@@ -1,7 +1,6 @@
 package com.aitextras.client;
 
-import com.aitextras.client.models.doors.*;
-import com.aitextras.client.models.exteriors.*;
+
 import com.aitextras.client.renderers.decor.*;
 import com.aitextras.client.renderers.decor.hudolinsupport.HudolinSupportBaseRenderer;
 import com.aitextras.client.renderers.decor.hudolinsupport.HudolinSupportTopRenderer;
@@ -16,50 +15,18 @@ import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
-import org.joml.Vector3f;
 
 import static com.aitextras.AITExtras.*;
 
 public class AITExtrasClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        registerClientAddonExteriors();
         blockEntityRendererRegister();
         BlockRenderLayerMap.INSTANCE.putBlock(AITExtrasBlocks.SEAL_BLOCK, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(AITExtrasBlocks.SEAL_SMALL_BLOCK, RenderLayer.getCutout());
     }
 
-    private void registerClientAddonExteriors() {
-        ECTO.setModel(new EctoExteriorModel()).toClient().register();
-        ECTO.setPortalWidth(1.15f);
-        ECTO.setPortalHeight(2.28f);
-        ECTO.toDoor().setModel(new EctoDoorModel(EctoDoorModel.getTexturedModelData().createModel())).toClient().register();
 
-        POSTBOX.setModel(new PostBoxExteriorModel()).toClient().register();
-        POSTBOX.setSonicItemTranslations(new Vector3f(0.15f, 1.122f, 0.94f));
-        POSTBOX.setPortalWidth(0.9f);
-        POSTBOX.setPortalHeight(2f);
-        POSTBOX.toDoor().setModel(new PostBoxDoorModel(PostBoxDoorModel.getTexturedModelData().createModel())).toClient().register();
-
-        VANILLA.setModel(new VanillaExteriorModel()).toClient().register();
-        VANILLA.setSonicItemTranslations(new Vector3f(0.7f, 1f, 3f));
-        VANILLA.setPortalWidth(1f);
-        VANILLA.setPortalHeight(2.5f);
-        VANILLA.toDoor().setModel(new VanillaDoorModel(VanillaDoorModel.getTexturedModelData().createModel())).toClient().register();
-//        VANILLA.toClient().hasTransparentDoors();
-
-        CLOCK.setModel(new ClockExteriorModel<>()).toClient().register();
-        CLOCK.setSonicItemTranslations(new Vector3f(0.7f, 1f, 2f));
-        CLOCK.setPortalWidth(1f);
-        CLOCK.setPortalHeight(2f);
-        CLOCK.toDoor().setModel(new ClockDoorModel(ClockDoorModel.getTexturedModelData().createModel())).toClient().register();
-
-        ORIGIN.setModel(new OriginExteriorModel()).toClient().register();
-        ORIGIN.setSonicItemTranslations(new Vector3f(0.7f, 1f, 2f));
-        ORIGIN.setPortalWidth(1.2f);
-        ORIGIN.setPortalHeight(2.4f);
-        ORIGIN.toDoor().setModel(new OriginDoorModel(OriginDoorModel.getTexturedModelData().createModel())).toClient().register();
-    };
     public static void blockEntityRendererRegister() {
         BlockEntityRendererFactories.register(AITExtrasBlockEntityTypes.EXTRAS_MONITOR_BLOCK_ENTITY_TYPE, ExtrasMonitorRenderer::new);
         BlockEntityRendererFactories.register(AITExtrasBlockEntityTypes.EXTRAS_SCREEN_MONITOR_BLOCK_ENTITY_TYPE, ExtrasScreenMonitorRenderer::new);
